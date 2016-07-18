263add421756d19baec488ffe3c8aae36464a724
Nikhil Dubbaka <info@technikh.com>
2016-07-18 14:32:46 -0400

    Adding examples index listing file.

---
+++ examples/index.php
@@ -0,0 +1,6 @@
+<ul>
+  <li><a href="quick-start.php">Quick start</a></li>
+  <li><a href="creditcard-authorize-and-capture.php">Authorize and capture a credit card.</a></li>
+  <li><a href="generic-transaction.php">Generic transaction</a></li>
+  <li><a href="multiple-transactions.php">Multiple transactions.</a></li>
+</ul>
