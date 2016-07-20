21ebdbed70f86e9ae613ec72cbcdefbaa6f7eefe
Nikhil Dubbaka <info@technikh.com>
2016-07-20 13:14:56 -0400

    code standards for unit tests.

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -2,14 +2,15 @@
 
 class BaseTest extends PHPUnit_Framework_TestCase
 {
-    public static $client;
+  public static $client;
     
-    public static function setUpBeforeClass(){
-      self::$client = new Payeezy_Client();
-      self::$client->setApiKey("y2sdDUJ0gGnxTyQFUGqDrutPHQ7LbeCS");
-      self::$client->setApiSecret("3472aaf343dce18899ed72af1c9f98f5c8b0912c0bfe3defe97cf2d158c0f96f");
-      self::$client->setMerchantToken("fdoa-0e8a514fecf2a834e0613becce6e98b30e8a514fecf2a834");
-      self::$client->setTokenUrl("https://api-qa.payeezy.com/v1/transactions/tokens");
-      self::$client->setUrl("https://api-qa.payeezy.com/v1/transactions");
-    }
+  public static function setUpBeforeClass()
+  {
+    self::$client = new Payeezy_Client();
+    self::$client->setApiKey("y2sdDUJ0gGnxTyQFUGqDrutPHQ7LbeCS");
+    self::$client->setApiSecret("3472aaf343dce18899ed72af1c9f98f5c8b0912c0bfe3defe97cf2d158c0f96f");
+    self::$client->setMerchantToken("fdoa-0e8a514fecf2a834e0613becce6e98b30e8a514fecf2a834");
+    self::$client->setTokenUrl("https://api-qa.payeezy.com/v1/transactions/tokens");
+    self::$client->setUrl("https://api-qa.payeezy.com/v1/transactions");
+  }
 }
--- tests/Payeezy/TokenTest.php
+++ tests/Payeezy/TokenTest.php
@@ -2,25 +2,27 @@
 
 class Payeezy_TokenTest extends BaseTest
 {
-    public function testAuthorize()
-    {
-      $transaction = new Payeezy_Token(parent::$client);
-      $response = $transaction->authorize([
+  public function testAuthorize()
+  {
+    $transaction = new Payeezy_Token(parent::$client);
+    $response = $transaction->authorize(
+        [
         "merchant_ref" => "Astonishing-Sale",
         "transaction_type" => "authorize",
         "method" => "token",
         "amount" => "200",
         "currency_code" => "USD",
         "token" => array(
-          "token_type" => "FDToken",
-          "token_data" => array(
-            "type" => "visa",
-            "value" => "2537446225198291",
-            "cardholder_name" => "JohnSmith",
-            "exp_date" => "1030"
-          )
+        "token_type" => "FDToken",
+        "token_data" => array(
+        "type" => "visa",
+        "value" => "2537446225198291",
+        "cardholder_name" => "JohnSmith",
+        "exp_date" => "1030"
         )
-      ]);
-      $this->assertEquals($response->validation_status, "success");
-    }
+        )
+        ]
+    );
+    $this->assertEquals($response->validation_status, "success");
+  }
 }
