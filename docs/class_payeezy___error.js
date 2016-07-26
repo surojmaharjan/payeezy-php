52855c2cece2e63b3c42298af168fb6e86a6db39
Nikhil Dubbaka <info@technikh.com>
2016-07-26 16:40:32 -0400

    Updating Docs.

--- docs/class_payeezy___error.js
+++ docs/class_payeezy___error.js
@@ -1,6 +1,7 @@
 var class_payeezy___error =
 [
-    [ "__construct", "class_payeezy___error.html#a64e5556aebe26ef160fd738be8549300", null ],
+    [ "__construct", "class_payeezy___error.html#ad73a724607843e8da1da0b3545ff7535", null ],
     [ "getHttpBody", "class_payeezy___error.html#a21fadee8d19a86c04b9222f2f448ef0b", null ],
-    [ "getHttpStatus", "class_payeezy___error.html#aa66c58c9fc9b387e36864bd9f0d6e72d", null ]
+    [ "getHttpStatus", "class_payeezy___error.html#aa66c58c9fc9b387e36864bd9f0d6e72d", null ],
+    [ "getJsonBody", "class_payeezy___error.html#ae8319dda214cb718a5353de0ccc11a45", null ]
 ];
--- docs/search/all_7.js
+++ docs/search/all_7.js
@@ -4,7 +4,7 @@ var searchData=
   ['getapisecret',['getApiSecret',['../class_payeezy___client.html#ad7fe6931c339c462e3be99d183a30f2e',1,'Payeezy_Client']]],
   ['gethttpbody',['getHttpBody',['../class_payeezy___error.html#a21fadee8d19a86c04b9222f2f448ef0b',1,'Payeezy_Error']]],
   ['gethttpstatus',['getHttpStatus',['../class_payeezy___error.html#aa66c58c9fc9b387e36864bd9f0d6e72d',1,'Payeezy_Error']]],
+  ['getjsonbody',['getJsonBody',['../class_payeezy___error.html#ae8319dda214cb718a5353de0ccc11a45',1,'Payeezy_Error']]],
   ['getmerchanttoken',['getMerchantToken',['../class_payeezy___client.html#ad0a46c71f31e408ce795514139736056',1,'Payeezy_Client']]],
-  ['gettokenurl',['getTokenUrl',['../class_payeezy___client.html#af4d9631b941479a42680a96afda40edc',1,'Payeezy_Client']]],
   ['geturl',['getUrl',['../class_payeezy___client.html#a5f7f80ef441454f68712b8f289a86cd9',1,'Payeezy_Client']]]
 ];
